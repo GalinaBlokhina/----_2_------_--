b5b50cc74e68a1f8d252a4aafea0f97d13989de4
BlokhinaGalina <galina-a@yandex.ru>
2023-02-04 19:05:30 +0300

    Updated Task_score_js file

--- Task_score_js.js
+++ Task_score_js.js
@@ -1,6 +1,7 @@
-let intro = 100
-let GIT = 120
-let JS = 140
-let average = (intro+GIT+JS)/3
-let studentName = 'Harry Potter'
-console.log(`GPA of ${studentName} is ${average}`)
+let intro = 100;
+let GIT = 120;
+let JS = 140;
+let average = (intro + GIT + JS) / 3;
+let studentName = "Harry";
+let surname = "Potter";
+console.log(`GPA of ${studentName.slice(0, 1)}. ${surname} is ${average}`);
